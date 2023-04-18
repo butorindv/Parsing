1b204c5a59892067a9c9eea14c98d389d7500c2f
butor <butorin_dv@mail.ru>
2023-04-18 14:56:00 +0300

    Добавил комменты в proxy_list.py

--- proxy_list.py
+++ proxy_list.py
@@ -1,18 +1,23 @@
 import requests
 
 
+# Функция читает файл с прокси-серверами (file_data) и возвращает список серверов
 def create_proxy_list(file_data):
     with open(file_data, 'r') as file:
         return file.read().split()
 
 
+# Функция создаёт словарь прокси для последующего использования
+# в requests в качестве параметра requests(url, proxies=proxy_for_request("адрес_прокси:порт"))
+# Если прокси с аутофикацией то формат (логин:пароль@адрес_прокси:порт)
 def proxy_for_request(proxy_string):
     return {
         "http": f"http://{proxy_string}",
-        "https": f"http://{proxy_string}",
+        "https": f"http://{proxy_string}"
     }
 
 
+# Проверка прокси на любом сайте + проверка сайта без прокси
 def proxy_checker(url, list_proxy):
     url_checker_non_proxy(url)
     for item in list_proxy:
@@ -24,6 +29,7 @@ def proxy_checker(url, list_proxy):
             print(f"ERROR: {url} - {req} - {item.split('@')[1]}")
 
 
+# проверка сайта без прокси
 def url_checker_non_proxy(url):
     req = ''
     try:
