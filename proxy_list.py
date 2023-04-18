3508fd46aa063a8fc93f7722d1d315da6191362d
butor <butorin_dv@mail.ru>
2023-04-18 15:51:49 +0300

    Добавил методы get_request и ip_useragent_checker

--- proxy_list.py
+++ proxy_list.py
@@ -1,10 +1,18 @@
 import requests
+from bs4 import BeautifulSoup
+import lxml
 
 
-# Функция читает файл с прокси-серверами (file_data) и возвращает список серверов
+# Функция читает файл с прокси-серверами и возвращает список серверов
 def create_proxy_list(file_data):
     with open(file_data, 'r') as file:
         return file.read().split()
+
+
+# Функция читает файл с бзер-агентами и возвращает список юзер-агентов
+def create_useragent_list(file_data):
+    with open(file_data, 'r') as file:
+        return file.read().split('\n')
 
 
 # Функция создаёт словарь прокси для последующего использования
@@ -29,7 +37,7 @@ def proxy_checker(url, list_proxy):
             print(f"ERROR: {url} - {req} - {item.split('@')[1]}")
 
 
-# проверка сайта без прокси
+# Проверка сайта без прокси
 def url_checker_non_proxy(url):
     req = ''
     try:
@@ -37,3 +45,16 @@ def url_checker_non_proxy(url):
         print(f"{url} - {req} - No proxy")
     except Exception:
         print(f"ERROR: {url} - No proxy")
+
+
+# Функция для запроса
+def get_request(url, headers_dict, proxy_server_dict=None):
+    return requests.get(url, headers=headers_dict, proxies=proxy_server_dict, timeout=20)
+
+
+# Проверка User-Agent и IP-адрес
+def ip_useragent_checker(headers_dict, proxy_server_dict=None):
+    req = requests.get('https://tools.browsernative.com/', headers=headers_dict, proxies=proxy_server_dict, timeout=20)
+    src = BeautifulSoup(req.text, 'lxml')
+    return f"IP: {src.find('span', class_='mono red copydata').text}\n" \
+           f"User-Agent: {src.find('span', class_='mono copydata').text}"
--- test_parsing.py
+++ test_parsing.py
@@ -1,12 +1,21 @@
-from proxy_list import create_proxy_list
-from proxy_list import proxy_checker
+from proxy_list import *
 
 list_proxy = create_proxy_list('datapool/proxy.txt')
+proxy_one = proxy_for_request(list_proxy[1])
+user_agents_list = create_useragent_list('datapool/user_agents_decktop.txt')
+headers = {"user-agent": f"{user_agents_list[100]}"}
 url_check_https = 'https://2ip.ru/'
 url_check_http = 'http://checkip.dyndns.org'
 url = 'https://vbankcenter.ru/contragent/1145256007577'
 url_proxy = 'https://proxy6.net'
 url_mail = 'https://mail.ru/'
 url_kinosal = 'https://forum.kinozal.me/index.php'
+url_check_useragent = 'https://ciox.ru/check-user-agent'
+url_check_ip_and_useragent = 'https://tools.browsernative.com/'
 
-proxy_checker(url, list_proxy)
+print(list_proxy)
+print(user_agents_list[0])
+
+# proxy_checker(url, list_proxy)
+# print(get_request(url_check_ip_and_useragent, headers, proxy_one).text)
+# print(ip_useragent_checker(headers, proxy_one))
